ac2cd446c3bbf478960eec3ea6231e5899729e8c
Casey Haralson <casey.haralson@gmail.com>
2016-01-06 16:47:01 -0800

    makes the messages look better, adds note about whether or not suspicious links were found, finds recipient address better, and finds attachments better

--- fishhook/gmail.js
+++ fishhook/gmail.js
@@ -14,7 +14,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                     senderDomain = null,
                     senderSubdomain = null,
                     recipientEmail = null,
-                    somethingSuspiciousFound = false;
+                    suspiciousLinkFound = false,
+                    suspiciousAttachmentFound = false;
 
 
                 // lets find the current block's sender
@@ -38,16 +39,18 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 
                 // lets find the recipient
-                var recipientElement = $(emailBlock).find('span.hb span.g2');
-                if (recipientElement != null && recipientElement.length > 0 && recipientElement[recipientElement.length - 1].attributes.hasOwnProperty('email')) {
-                    var recipientEmail = recipientElement[recipientElement.length - 1].attributes['email'].value;
+                var recipientElementMe = $(emailBlock).find('span.hb span.g2:contains("me")');
+                var recipientElement = $(emailBlock).find('span.hb span.g2'); // in case I'm sending the email
+                if (recipientElementMe != null && recipientElementMe.length == 1) recipientElement = recipientElementMe;
+                if (recipientElement != null && recipientElement.length > 0 && recipientElement[0].attributes.hasOwnProperty('email')) {
+                    var recipientEmail = recipientElement[0].attributes['email'].value;
                 }
 
 
-                // lets look at all the links in the email block
+                // lets look at all the links in the email block (skip links inside calendar block and header)
                 // and make sure it comes from the correct domain
                 // or that it's a mailto link to the sender or recipient
-                var linksInBlock = $(emailBlock).find('a');
+                var linksInBlock = $(emailBlock).find('a').not($(emailBlock).find('.aHl a')).not($(emailBlock).find('.iv a')); // header could be gE/iv/gt
                 if (linksInBlock != null && linksInBlock.length > 0) {
                     for (var iLink = 0; iLink < linksInBlock.length; iLink++) {
                         var link = linksInBlock[iLink],
@@ -72,27 +75,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                         if (linkHref != null && linkHref.length > 0) $(link).addClass('fishhook-suspicious-link');
 
                         // compare the link domain to the sender domain and remove the suspicious flag if they match
-                        if (linkDomain != null && senderDomain === linkDomain) {
+                        if (linkDomain != null && senderDomain.toLowerCase() == linkDomain.toLowerCase()) {
                             $(link).removeClass('fishhook-suspicious-link');
+                            $(link).addClass('fishhook-link');
                         }
 
                         // if the link is a mailto for the sender or recipient
-                        // remove the suspicious flag
-                        if (linkHref != null && (linkHref == 'mailto:' + senderEmail || linkHref == 'mailto:' + recipientEmail)) {
+                        // remove the suspicious flag and make it a regular link
+                        if (linkHref != null && (linkHref.toLowerCase() == 'mailto:' + senderEmail.toLowerCase() || linkHref.toLowerCase() == 'mailto:' + recipientEmail.toLowerCase())) {
                             $(link).removeClass('fishhook-suspicious-link');
+                            $(link).addClass('fishhook-link');
                         }
 
                         // if the link parent has a download url, it is an attachment
                         // move the suscpicious flag to the parent
-                        if (linkParent != null && linkParent.attributes.hasOwnProperty('download_url')) {
+                        if (linkParent != null && (linkParent.attributes.hasOwnProperty('download_url') || $(linkParent).hasClass('aZo'))) {
                             $(link).removeClass('fishhook-suspicious-link');
                             $(linkParent).addClass('fishhook-suspicious-attachment');
+                            suspiciousAttachmentFound = true; // all attachments are suspicious
                         }
 
 
                         // did we find something suspicious
                         if ($(link).hasClass('fishhook-suspicious-link')) {
-                            somethingSuspiciousFound = true;
+                            suspiciousLinkFound = true;
                         }
                     }
                 }
@@ -101,27 +107,40 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 // flag the email body depending on what we found above
                 var emailBodyElement = $(emailBlock).find('div.a3s');
                 if (emailBodyElement != null) {
-                    if (somethingSuspiciousFound) $(emailBodyElement).addClass('fishhook-suspicious-email-body');
-                    else $(emailBodyElement).addClass('fishhook-email-body');
-                }
-
-
-                // set a reminder to check the email from address
-                if (senderEmailBlock != null && $(senderEmailBlock).find('span.fishhook-reminder').length == 0) {
-                    $(senderEmailBlock).prepend('<span class="fishhook-reminder">Make sure the email address looks good.</span>');
+                    if (!$(emailBodyElement).hasClass('fishhook-suspicious-email-body') && !$(emailBodyElement).hasClass('fishhook-email-body')) {
+                        if (suspiciousLinkFound) {
+                            $(emailBodyElement).addClass('fishhook-suspicious-email-body');
+                            $(emailBodyElement).prepend('<div class="fishhook-suspicious-email-body-message"><span>Suspicious links found!</span></div>')
+                        }
+                        else {
+                            $(emailBodyElement).addClass('fishhook-email-body');
+                            $(emailBodyElement).prepend('<div class="fishhook-email-body-message"><span>No suspicious links found!</span></div>')
+                        }
+                    }
                 }
 
 
                 // if there is an attachment, set a reminder to check it
-                if ($(emailBlocks).find('span.fishhook-suspicious-attachment').length > 0) {
-                    var attachmentsArea = $(emailBlocks).find('div>:contains("Attachments area")');
+                if ($(emailBlock).find('span.fishhook-suspicious-attachment').length > 0) {
+                    var attachmentsArea = $(emailBlock).find('div>:contains("Attachments area"), div>:contains(" Attachments")');
                     if (attachmentsArea != null && attachmentsArea.length > 0) {
                         for (var iAttachmentsArea = 0; iAttachmentsArea < attachmentsArea.length; iAttachmentsArea++) {
                             var area = attachmentsArea[iAttachmentsArea];
-                            if ($(area.parentElement).find('span.fishhook-reminder').length == 0) {
-                                $(area.parentElement).prepend('<span class="fishhook-reminder">Make sure the attachments are ok file types.');
+                            if ($(emailBlock).find('span.fishhook-reminder-attachments').length == 0) {
+                                $(area.parentElement).prepend('<span class="fishhook-reminder-attachments">** Make sure the attachment\'s extension (.jpg, .png, etc) is familiar and expected. **</span>');
                             }
                         }
+                    }
+                }
+
+
+                // set a reminder to check the email from address
+                if (senderEmailBlock != null && ($(senderEmailBlock).find('span.fishhook-reminder-sender-address').length == 0 && $(senderEmailBlock).find('span.fishhook-reminder-sender-address-suspicious').length == 0)) {
+                    if (suspiciousLinkFound || suspiciousAttachmentFound) {
+                        $(senderEmailBlock).prepend('<span class="fishhook-reminder-sender-address-suspicious">** Make sure the email address below is from who you expect. **</span>');
+                    }
+                    else {
+                        $(senderEmailBlock).prepend('<span class="fishhook-reminder-sender-address">** Make sure the email address below is from who you expect. **</span>');
                     }
                 }
 
